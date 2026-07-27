74ba27fe32597d93903c0f4facda7f16895f8378
Kostas Alvertis <konstantinos.alvertis@gmail.com>
2026-07-27 19:02:56 +0100

    WIP: simple dbgs trace for R_PPC64_REL24 edges

--- llvm/lib/ExecutionEngine/JITLink/ELF_ppc64.cpp
+++ llvm/lib/ExecutionEngine/JITLink/ELF_ppc64.cpp
@@ -359,21 +359,12 @@ private:
     case ELF::R_PPC64_REL24:
       Kind = ppc64::RequestCall;
       // Determining a target is external or not is deferred in PostPrunePass.
-      // We assume branching to local entry by default, since in PostPrunePass,
-      // we don't have any context to determine LocalEntryOffset. If it finally
-      // turns out to be an external call, we'll have a stub for the external
-      // target, the target of this edge will be the stub and its addend will be
-      // set 0.
       Addend += ELF::decodePPC64LocalEntryOffset((*ObjSymbol)->st_other);
-      // DEBUG: print every R_PPC64_REL24 edge being created
-      DEBUG_WITH_TYPE("jitlink-ppc64-rel24",
-        dbgs() << "PPC64-REL24-EDGE: offset=0x"
-               << formatv("{0:x}", Offset)
-               << " sym=" << (*ObjSymbol)->getName().get()
-               << " in_section=" << BlockToFix.getSection().getName()
-               << " block_addr=0x"
-               << formatv("{0:x}", BlockToFix.getAddress().getValue())
-               << "\n");
+      // DEBUG: trace every R_PPC64_REL24 edge
+      dbgs() << "PPC64-REL24: offset=0x" << Twine::utohexstr(Offset)
+             << " section=" << BlockToFix.getSection().getName()
+             << " block=0x" << Twine::utohexstr(BlockToFix.getAddress().getValue())
+             << "\n";
       break;
     case ELF::R_PPC64_REL64:
       Kind = ppc64::Delta64;
