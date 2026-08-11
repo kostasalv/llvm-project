cdcd74444003874f7dbd32a291f1e70898189c3e
Kostas Alvertis <konstantinos.alvertis@gmail.com>
2026-08-11 18:18:37 +0100

    bolt/PPC64: widen DBG8 to all Z41/1 insertBasicBlocks

--- bolt/lib/Passes/LongJmp.cpp
+++ bolt/lib/Passes/LongJmp.cpp
@@ -777,18 +777,17 @@ Error LongJmpPass::relax(BinaryFunction &Func, bool &Modified) {
        Insertions) {
     if (!Elmt.second)
       continue;
-    // DBG8: trace insertBasicBlocks calls where InsertionPoint is in hot BBAddr
-    // range 0x17800040..0x178000f0 (which maps to the 0x1367f760 region)
-    if (BC.isPPC64() && BBAddresses.count(Elmt.first)) {
-      uint64_t IP = BBAddresses[Elmt.first];
-      if (IP >= 0x17800040 && IP <= 0x178000f0) {
-        errs() << "BOLT PPC64 LongJmp DBG8: insertBasicBlocks"
-               << " Func=" << Func.getPrintName()
-               << " IPaddr=0x" << Twine::utohexstr(IP)
-               << " IPindex=" << Elmt.first->getIndex()
-               << " totalBBs=" << Func.size()
-               << "\n";
-      }
+    // DBG8: trace ALL insertBasicBlocks for _Z41/1 to find which BB is InsertionPoint
+    if (BC.isPPC64() &&
+        Func.getPrintName().find("_Z41__static_init") != std::string::npos &&
+        Func.getPrintName().find("/1(") != std::string::npos &&
+        BBAddresses.count(Elmt.first)) {
+      errs() << "BOLT PPC64 LongJmp DBG8: insertBasicBlocks"
+             << " Func=" << Func.getPrintName()
+             << " IPaddr=0x" << Twine::utohexstr(BBAddresses[Elmt.first])
+             << " IPindex=" << Elmt.first->getIndex()
+             << " totalBBs=" << Func.size()
+             << "\n";
     }
     std::vector<std::unique_ptr<BinaryBasicBlock>> NewBBs;
     NewBBs.emplace_back(std::move(Elmt.second));
