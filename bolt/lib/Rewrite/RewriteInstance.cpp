714bb8fb9bea6cb476312828c722f6261e90acd8
Kostas Alvertis <konstantinos.alvertis@gmail.com>
2026-09-12 17:14:53 +0100

    [TEMP AUDIT] count total relocations found per readDynamicRelocations call

--- bolt/lib/Rewrite/RewriteInstance.cpp
+++ bolt/lib/Rewrite/RewriteInstance.cpp
@@ -3307,6 +3307,15 @@ void RewriteInstance::readDynamicRelocations(const SectionRef &Section,
            << ":\n";
   });
 
+  {
+    unsigned RelCount = 0;
+    for (const RelocationRef &R : Section.relocations())
+      (void)R, ++RelCount;
+    BC->errs() << "AUDIT readDynamicRelocations: section="
+               << cantFail(Section.getName()) << " relocCount=" << RelCount
+               << "\n";
+  }
+
   for (const RelocationRef &Rel : Section.relocations()) {
     uint32_t JmpRelocationIndex = Relocation::NoJmpRelocationIndex;
     if (IsJmpRel) {
